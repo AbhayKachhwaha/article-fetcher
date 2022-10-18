fc92710c30951066159f31d5ba156d00db80dd80
AbhayKachhwaha <abhay.kachhwah09@gmail.com>
2022-10-18 15:40:36 -0400

    indentation

--- src/main/java/com/topl/interview/articlefetcher/models/Source.java
+++ src/main/java/com/topl/interview/articlefetcher/models/Source.java
@@ -4,18 +4,13 @@ public class Source {
     private String name;
     private String url;
 
-    
     public Source() {
     }
-
-    
 
     public Source(String name, String url) {
         this.name = name;
         this.url = url;
     }
-
-
 
     public String getName() {
         return name;
